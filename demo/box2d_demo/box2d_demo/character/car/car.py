a975b31e9d890ae28e5aae325477767331f1a604
kfields <kurtisfields@gmail.com>
2026-09-13 17:56:30 -0600

    small cleanup

--- demo/box2d_demo/box2d_demo/character/car/car.py
+++ demo/box2d_demo/box2d_demo/character/car/car.py
@@ -6,7 +6,6 @@ import crunge.box2d as box2d
 from crunge.engine.loader.sprite.sprite_loader import SpriteLoader
 from crunge.engine.builder.sprite import CollidableSpriteBuilder
 
-from crunge.engine.d2.sprite import SpriteVu
 from crunge.engine.d2.entity import EntityGroup2D, Entity2D, DynamicEntity2D
 from crunge.engine.d2.physics import BoxGeom, BallGeom
 from crunge.engine.d2.physics import globe as physics_globe
